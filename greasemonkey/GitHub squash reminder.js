6b2ac50ca9b1944896bee3cea3cf5408038084da
George Gillams <georgegillams@skyscanner.net>
2018-05-26 10:14:27 +0100

    Update github merge script

--- greasemonkey/GitHub squash reminder.js
+++ greasemonkey/GitHub squash reminder.js
@@ -3,8 +3,8 @@
 // @namespace   urn://https://www.georgegillams.co.uk/api/greasemonkey/github_squash_reminder
 // @include     *github.com*
 // @exclude     none
-// @version     1
-// @description:en	Adds an reminder to squash PRs before merging
+// @version     2
+// @description:en	Adds an reminder to squash PRs that have > 1 commit
 // @grant    		none
 // ==/UserScript==
 
@@ -15,26 +15,33 @@ function addReminder() {
   if (lastModifiedPr === prId) {
     return;
   } else {
-    let allElements = document.getElementsByTagName("DIV");
-    for (let i = 0; i < allElements.length; i += 1) {
-      let element = allElements[i];
-      if (element.className === "merge-message") {
-        element.innerHTML =
-          `<div style="display: block;margin-bottom: -2rem;text-decoration: none;font-weight: bold;font-size: 1.5rem;font-family: Quattrocento Sans,sans-serif;color: #e02626;">REMEMBER TO SQUASH!</div> <br/> <br/>` +
-          element.innerHTML;
+    let commitCount = parseInt(
+      document.getElementById("commits_tab_counter").textContent
+    );
+    console.log(commitCount);
+    if (commitCount !== 1) {
+      let allElements = document.getElementsByTagName("DIV");
+      for (let i = 0; i < allElements.length; i += 1) {
+        let element = allElements[i];
+        if (element.className === "merge-message") {
+          element.innerHTML =
+            `<div style="display: block;margin-bottom: -2rem;text-decoration: none;font-weight: bold;font-size: 1.5rem;font-family: Quattrocento Sans,sans-serif;color: #e02626;">REMEMBER TO SQUASH!</div> <br/> <br/>` +
+            element.innerHTML;
+        }
       }
-    }
-    allElements = document.getElementsByTagName("BUTTON");
-    for (let i = 0; i < allElements.length; i += 1) {
-      let element = allElements[i];
-      if (
-        element.textContent &&
-        element.textContent.includes("Merge pull request")
-      ) {
-        element.textContent = "MERGE ONLY ONCE SQUASHED!";
-        element.style.backgroundImage =
-          "linear-gradient(-180deg, #e02626 0%, #9F2D27 90%)";
-        element.style.color = "white";
+      allElements = document.getElementsByTagName("BUTTON");
+      for (let i = 0; i < allElements.length; i += 1) {
+        let element = allElements[i];
+        if (
+          element.textContent &&
+          element.textContent.includes("Merge pull request")
+        ) {
+          element.disabled = true;
+          element.textContent = "MERGE ONLY ONCE SQUASHED!";
+          element.style.backgroundImage =
+            "linear-gradient(-180deg, #e02626 0%, #9F2D27 90%)";
+          element.style.color = "white";
+        }
       }
     }
     lastModifiedPr = prId;
