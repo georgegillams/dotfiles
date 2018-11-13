e449a29bf87f319f2a9731eefa085658343a48e1
George Gillams <george.gillams@skyscanner.net>
2018-11-13 08:30:58 +0000

    Update image-optim script

--- image-optim.js
+++ image-optim.js
@@ -1,35 +1,18 @@
-/*
- * Backpack - Skyscanner's Design System
- *
- * Copyright 2018 Skyscanner Ltd
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/* eslint-disable no-console */
-
 const {execSync} = require('child_process');
 const prompt = require('prompt');
 const fs = require('fs');
 const path = require('path');
 
+// Note that for `git status -s` to identify an image file, it must be tracked. New files will not be included until they are added.
 const optimiseImages = async () => {
   const changedFiles = execSync('git status -s | cut -c4-').toString().split('\n').filter(s => s !== '');
   changedFiles.forEach(f => {
-    if(f.match(/(\.|\/)(gif|jpe?g|png)$/i)) {
+    if(f.match(/(\.|\/)(gif|jpe?g|png)"?$/i)) {
+      console.log(`optimising image file 🌄 --> ${f}`)
       execSync(`imageoptim ${f}`)
     }
   })
+  console.log(``);
 };
 
 optimiseImages();
