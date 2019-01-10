82d64d9352deb7ffa6de26810404366ed87b6925
George Gillams <george.gillams@skyscanner.net>
2019-01-10 08:13:06 +0000

    Update brew install script

--- brew_install.sh
+++ brew_install.sh
@@ -97,7 +97,9 @@ npm login
 
 cd ~/Documents && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd ~/Documents && git clone https://github.com/georgegillams/dotfiles.git
-cp dotfiles/.vimrc ~/.vimrc && cp dotfiles/.zshrc ~/.zshrc
+cp dotfiles/.vimrc ~/.vimrc
+cp dotfiles/.zshrc ~/.zshrc
+cp dotfiles/.tmux.conf ~/.tmux.conf
 . ~/.zshrc
 vim +"source ~/.vimrc" +PluginInstall +qall
 defaults write com.apple.finder AppleShowAllFiles YES
