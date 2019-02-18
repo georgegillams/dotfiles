c2b0b3dfd392c678bd61b929f3f52232d63a3e81
George Gillams <george.gillams@skyscanner.net>
2019-02-18 09:47:52 +0000

    Update brew install script

--- brew_install.sh
+++ brew_install.sh
@@ -87,27 +87,36 @@ nvm use 10.13.0
 # install NVM and RVM
 \curl -sSL https://get.rvm.io | bash -s stable
 
-# Set up Git SSH
-ssh-keygen -t rsa -b 4096 -C "george.gillams@skyscanner.net"
-echo "Now go to Github, select Add SSH key and paste the public file contents"
-pbcopy < ~/.ssh/id_rsa.pub
-sleep 10
-echo "Press any key once complete"
+# Set up Krypt.co SSH/GPG keys:
+echo "Enter dev mode in the app to get started"
+curl https://krypt.co/kr | sh
+kr pair
+kr github
+read
+kr codesign
 read
 
-# Set up Git commit signing
-gpg --gen-key
-echo "Configuring git to use the new GPG key"
-gpg --list-secret-keys --keyid-format LONG
-echo "Please copy the sec key and enter it"
-read keyLong
-git config --global user.signingkey $(keyLong)
-git config --global commit.gpgSign true
-echo "Now go to Github, select New GPG key and paste the exported data"
-gpg --export -a $(keyLong) | pbcopy
-sleep 10
-echo "Press any key once complete"
-read
+# # Set up Git SSH
+# ssh-keygen -t rsa -b 4096 -C "george.gillams@skyscanner.net"
+# echo "Now go to Github, select Add SSH key and paste the public file contents"
+# pbcopy < ~/.ssh/id_rsa.pub
+# sleep 10
+# echo "Press any key once complete"
+# read
+
+# # Set up Git commit signing
+# gpg --gen-key
+# echo "Configuring git to use the new GPG key"
+# gpg --list-secret-keys --keyid-format LONG
+# echo "Please copy the sec key and enter it"
+# read keyLong
+# git config --global user.signingkey $(keyLong)
+# git config --global commit.gpgSign true
+# echo "Now go to Github, select New GPG key and paste the exported data"
+# gpg --export -a $(keyLong) | pbcopy
+# sleep 10
+# echo "Press any key once complete"
+# read
 
 npm login
 
