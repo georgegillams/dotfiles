8555e2066d0acf51aedbbfadfd1fa410dc527012
George Gillams <george.gillams@skyscanner.net>
2019-05-03 16:12:36 +0100

    Update gm script

--- greasemonkey/Github WIP Reminder.js
+++ greasemonkey/Github WIP Reminder.js
@@ -3,7 +3,7 @@
 // @namespace   urn://https://www.georgegillams.co.uk/greasemonkey/github_WIP_reminder
 // @include     *github.com*
 // @exclude     none
-// @version     1
+// @version     2
 // @description:en	Adds an reminder to not review WIP PR's
 // @description	Adds an reminder to not review WIP PR's
 // @grant    		none
@@ -20,24 +20,27 @@ function addReminder() {
     for (let i = 0; i < allElements.length; i += 1) {
       let element = allElements[i];
       if (element.innerText.includes('WIP')) {
-        console.log(`wip`);
-        let newElement = document.createElement('div');
-        newElement.innerText = `HEY THIS IS WIP!`;
-        newElement.style.backgroundColor = 'rgb(214, 11, 0)';
-        newElement.style.color = 'white';
-        newElement.style.fontSize = '2rem';
-        newElement.style.position = 'fixed';
-        newElement.style.left = 0;
-        newElement.style.top = 0;
-        newElement.style.zIndex = 40000;
-        newElement.style.width = '100vw';
-        newElement.style.height = '5rem';
-        newElement.style.padding = '1rem';
-        newElement.style.display = 'flex';
-        newElement.style.alignItems = 'center';
-        newElement.style.justifyContent = 'center';
-
-        element.appendChild(newElement);
+          console.log(`element`, element)
+        console.log(`element.parentElement.parentElement`, element.parentElement.parentElement);
+        if (element.parentElement.parentElement.className.includes("labels css-truncate")) {
+          console.log(`wip`);
+          let newElement = document.createElement('div');
+          newElement.innerText = `HEY THIS IS WIP!`;
+          newElement.style.backgroundColor = 'rgb(214, 11, 0)';
+          newElement.style.color = 'white';
+          newElement.style.fontSize = '2rem';
+          newElement.style.position = 'fixed';
+          newElement.style.left = 0;
+          newElement.style.top = 0;
+          newElement.style.zIndex = 40000;
+          newElement.style.width = '100vw';
+          newElement.style.height = '5rem';
+          newElement.style.padding = '1rem';
+          newElement.style.display = 'flex';
+          newElement.style.alignItems = 'center';
+          newElement.style.justifyContent = 'center';
+          element.appendChild(newElement);
+        }
       }
     }
   }
