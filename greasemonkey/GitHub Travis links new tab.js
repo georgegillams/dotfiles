3eb6dfdd9fd611724832ecf5eeaa723b6092f601
George Gillams <george.gillams@skyscanner.net>
2019-09-11 09:57:11 +0100

    [NO-JIRA] Prevent new tab link error

--- greasemonkey/GitHub Travis links new tab.js
+++ greasemonkey/GitHub Travis links new tab.js
@@ -4,7 +4,7 @@
 // @include     *github.com*
 // @include     *github.skyscannertools.net*
 // @exclude     none
-// @version     5
+// @version     6
 // @description:en	Adds rel=no_opener and taget=blank to github travis links on Github
 // @grant    		none
 // @description   	Adds rel=no_opener and taget=blank to github travis links on Github
@@ -14,7 +14,7 @@ function addTarget() {
   const allElements = document.getElementsByTagName("*");
   for (let i = 0; i < allElements.length; i += 1) {
     let element = allElements[i];
-    if (element.href && (element.href.includes("travis") || element.href.includes("jenkins"))) {
+    if (element.href && element.href.includes && (element.href.includes("travis") || element.href.includes("jenkins"))) {
       element.target = "_blank";
       element.rel = "noopener noreferrer";
     }
