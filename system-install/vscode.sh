a31e9784844374109279a801cbaa9ba9e6c48cd9
George Gillams <george.gillams@skyscanner.net>
2020-05-18 06:14:26 +0100

    [NO-JIRA] Update vscode settings

--- system-install/vscode.sh
+++ system-install/vscode.sh
@@ -1,18 +1,21 @@
 # VSCode extensions
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension formulahendry.auto-close-tag
-code --install-extension cssho.vscode-svgviewer
-code --install-extension wayou.vscode-todo-highlight
-code --install-extension wmaurer.change-case
+code --install extension arcticicestudio.nord-visual-studio-code
+code --install extension dnlytras.nord-wave
+code --install extension emmanuelbeziat.vscode-great-icons
+code --install extension vscode-icons-team.vscode-icons
+code --install-extension adamwalzer.scss-lint
+code --install-extension christian-kohler.npm-intellisense
 code --install-extension chrmarti.regex
-code --install-extension firefox-devtools.vscode-firefox-debug
+code --install-extension cssho.vscode-svgviewer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
-code --install-extension christian-kohler.npm-intellisense
 code --install-extension esbenp.prettier-vscode
-code --install-extension adamwalzer.scss-lint
-code --install-extension kasik96.swift
-code --install-extension vscodevim.vim
+code --install-extension firefox-devtools.vscode-firefox-debug
 code --install-extension flowtype.flow-for-vscode
-
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension kasik96.swift
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vscodevim.vim
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension wmaurer.change-case
