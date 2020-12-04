9383215d98a9218d2ce6efef37aa26dc2a2d44ac
George Gillams <george.gillams@skyscanner.net>
2020-12-04 20:20:57 +0000

    [NO-JIRA] Update brew install script

--- system-install/mas.sh
+++ system-install/mas.sh
@@ -4,6 +4,7 @@ read
 
 sudo chown -R $(whoami) /usr/local/lib/pkgconfig
 
+mas install 1176895641 # Spark Email
 mas install 1236045954 # Canary Mail
 mas install 1278508951 # Trello
 mas install 409183694 # Keynote
