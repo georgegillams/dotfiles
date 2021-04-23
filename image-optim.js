7c7527baeb1a9f2e7a31dfbe0ec9eb4a1e1cb4d3
George Gillams <george.gillams@skyscanner.net>
2021-04-23 16:23:38 +0100

    [NO-JIRA] Add Snapshots to image-optim exclusion

--- image-optim.js
+++ image-optim.js
@@ -1,22 +1,29 @@
-const {execSync} = require('child_process');
-const prompt = require('prompt');
-const fs = require('fs');
-const path = require('path');
+const { execSync } = require("child_process");
+const prompt = require("prompt");
+const fs = require("fs");
+const path = require("path");
 
 // Note that for `git status -s` to identify an image file, it must be tracked. New files will not be included until they are added.
 const optimiseImages = async () => {
-  const changedFiles = execSync('git status -s | cut -c4-').toString().split('\n').filter(s => s !== '');
+  const changedFiles = execSync("git status -s | cut -c4-")
+    .toString()
+    .split("\n")
+    .filter((s) => s !== "");
   const filesToOptimise = [];
-  changedFiles.forEach(f => {
-    if(f.includes("SnapshotTest") || f.includes('backstop_data')){
-    console.log(`Skipping snapshot test file 🌇 --> ${f} `);
-    }else if(f.match(/(\.|\/)(gif|jpe?g|png)"?$/i)) {
-      console.log(`optimising image file 🌄 --> ${f}`)
+  changedFiles.forEach((f) => {
+    if (
+      f.includes("Snapshots") ||
+      f.includes("SnapshotTest") ||
+      f.includes("backstop_data")
+    ) {
+      console.log(`Skipping snapshot test file 🌇 --> ${f} `);
+    } else if (f.match(/(\.|\/)(gif|jpe?g|png)"?$/i)) {
+      console.log(`optimising image file 🌄 --> ${f}`);
       filesToOptimise.push(f);
     }
-  })
-  if(filesToOptimise.length > 0){
-  execSync(`imageoptim ${filesToOptimise.join(' ')}`)
+  });
+  if (filesToOptimise.length > 0) {
+    execSync(`imageoptim ${filesToOptimise.join(" ")}`);
   }
   console.log(``);
 };
