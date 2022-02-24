c2c13bf157096ea9da44b4b4dd8956e0a762ad5f
George Gillams <code@georgegillams.co.uk>
2022-02-24 13:26:32 +0000

    chore: Update ZSHRC

--- prepend-ticket.js
+++ prepend-ticket.js
@@ -1,32 +1,32 @@
 /* eslint-disable no-console */
 
-const fs = require('fs');
-const { execSync } = require('child_process');
+const fs = require("fs");
+const { execSync } = require("child_process");
 
-const BRANCH_TICKET_REGEX = /([A-Z][A-Z][A-Z][A-Z]?-[0-9]+)[\-\/].*/g
+const BRANCH_TICKET_REGEX = /([A-Z]+-[0-9]+)[\-\/].*/g;
 
 const getCurrentBranchName = () => {
-   return execSync(' git symbolic-ref --short HEAD').toString();
-}
+  return execSync(" git symbolic-ref --short HEAD").toString();
+};
 
 const getAppendedBranchName = () => {
   const commitMessage = process.argv[2];
-  if(!commitMessage) {
-    return ""; 
+  if (!commitMessage) {
+    return "";
   }
   const branchName = getCurrentBranchName();
-  if(!branchName) {
-    return commitMessage; 
+  if (!branchName) {
+    return commitMessage;
   }
 
   const match = BRANCH_TICKET_REGEX.exec(branchName);
-  const changeType = process.argv[3] || 'feat';
-  if(match){
-    return `${changeType}(${match[1]}): ${commitMessage}`
-  }else{
-    return `${changeType}: ${commitMessage}`
+  const changeType = process.argv[3] || "feat";
+  if (match) {
+    return `${changeType}(${match[1]}): ${commitMessage}`;
+  } else {
+    return `${changeType}: ${commitMessage}`;
   }
-}
+};
 
-const newBranchName = getAppendedBranchName()
+const newBranchName = getAppendedBranchName();
 console.log(newBranchName);
