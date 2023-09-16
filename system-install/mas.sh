a2c3ecabd77e6addea2491b9f48b98fe7ae4b2a7
George Gillams <code@georgegillams.co.uk>
2023-09-16 12:49:28 +0100

    fix: Update mac app store apps

--- system-install/mas.sh
+++ system-install/mas.sh
@@ -5,10 +5,13 @@ read
 sudo chown -R $(whoami) /usr/local/lib/pkgconfig
 
 mas install 1254981365 # Contrast — Color Accessibility
+mas install 1470584107 # Dato
 mas install 1444383602 # GoodNotes 6
 mas install 1176895641 # Spark Email
 mas install 409183694 # Keynote
 mas install 409201541 # Pages
 mas install 409203825 # Numbers
 mas install 937984704 # Amphetamine
+mas install 1524540481 # Widgy Widgets
+mas install 2125818515 # Contraster
 mas install 497799835 # XCode
