390053d5b9ef0f012a7f57f019fbdb7ec5d06c93
George Gillams <code@georgegillams.co.uk>
2023-09-19 16:58:12 +0100

    chore: Update brew install script

--- system-install/mas.sh
+++ system-install/mas.sh
@@ -7,7 +7,6 @@ sudo chown -R $(whoami) /usr/local/lib/pkgconfig
 mas install 1254981365 # Contrast — Color Accessibility
 mas install 1470584107 # Dato
 mas install 1444383602 # GoodNotes 6
-mas install 1176895641 # Spark Email
 mas install 409183694 # Keynote
 mas install 409201541 # Pages
 mas install 409203825 # Numbers
