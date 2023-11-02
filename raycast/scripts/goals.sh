a41c14c7724d11c96b003629d05285facdcc6d40
George Gillams <code@georgegillams.co.uk>
2023-11-02 21:16:32 +0000

    chore: Update ZSHRC

---
+++ raycast/scripts/goals.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Goals and dreams
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ./goals.png
+
+# Documentation:
+# @raycast.author George Gillams
+# @raycast.authorURL https://www.georgegillams.co.uk/
+
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/2_Areas/Work/Typeform/Performance/Promotion/Career framework competencies assesement.numbers"
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/2_Areas/Growth, Goals, and Dreams/Dreams by Dimensions.pages"
