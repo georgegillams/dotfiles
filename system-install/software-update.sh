03728c8bec4d5638e90172f95e238fd51b3b526e
George Gillams <code@georgegillams.co.uk>
2023-12-29 14:52:07 +0000

    feat: Update system install scripts (#7)

---
+++ system-install/software-update.sh
@@ -0,0 +1,4 @@
+echo "Install any system updates"
+open "x-apple.systempreferences:com.apple.systempreferences.GeneralSettings"
+echo "Press any key once complete"
+read
---
+++ system-install/system-preferences.sh
@@ -0,0 +1,24 @@
+echo "\n\nAdd fingerprints"
+open "x-apple.systempreferences:com.apple.Touch-ID-Settings.extension"
+echo "\nPress any key once complete"
+read
+echo "\n\nAdd bluetooth devices"
+open "x-apple.systempreferences:com.apple.BluetoothSettings"
+echo "\nPress any key once complete"
+read
+echo "\n\nSet keyboard repeat speeds\nDisable spotlight shortcut\nDisable caps-lock for both keyboards"
+open "x-apple.systempreferences:com.apple.Keyboard-Settings.extension"
+echo "\nPress any key once complete"
+read
+echo "\n\nTurn on auto appearance\nTurn on permanent scroll bars"
+open "x-apple.systempreferences:com.apple.Appearance-Settings.extension"
+echo "\nPress any key once complete"
+read
+echo "\n\nMake dock auto hide\nDisable recent items\nTurn off automatically rearranging spaces"
+open "x-apple.systempreferences:com.apple.Desktop-Settings.extension"
+echo "\nPress any key once complete"
+read
+echo "\n\nEnable mouse right-click"
+open "x-apple.systempreferences:com.apple.Mouse-Settings.extension"
+echo "\nPress any key once complete"
+read
