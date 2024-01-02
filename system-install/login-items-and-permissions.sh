112c1338b1fc8fb28fd63117cef2fa71f61fa9eb
George Gillams <code@georgegillams.co.uk>
2024-01-02 21:54:37 +0000

    chore: Update brew install script

--- system-install/login-items-and-permissions.sh
+++ system-install/login-items-and-permissions.sh
@@ -6,7 +6,3 @@ echo "\n\nGive apps screen-recording permissions"
 open "x-apple.systempreferences:com.apple.settings.PrivacySecurity.extension"
 echo "\nPress any key once complete"
 read
-echo "\n\nBlock Spotlight from indexing Google Drive or files directory"
-open "x-apple.systempreferences:com.apple.Siri-Settings.extension"
-echo "\nPress any key once complete"
-read
--- system-install/system-preferences.sh
+++ system-install/system-preferences.sh
@@ -1,21 +1,25 @@
-echo "\n\nAdd fingerprints"
-open "x-apple.systempreferences:com.apple.Touch-ID-Settings.extension"
-echo "\nPress any key once complete"
-read
 echo "\n\nAdd bluetooth devices"
 open "x-apple.systempreferences:com.apple.BluetoothSettings"
-echo "\nPress any key once complete"
-read
-echo "\n\nSet keyboard repeat rate to 8/8\nSet delay until repeat to 6/6\nEnable Keyboard Navigation\nOpen Keyboard Shortcuts\nDisable spotlight shortcut\nDisable caps-lock for both keyboards"
-open "x-apple.systempreferences:com.apple.Keyboard-Settings.extension"
 echo "\nPress any key once complete"
 read
 echo "\n\nTurn on auto appearance\nTurn on permanent scroll bars"
 open "x-apple.systempreferences:com.apple.Appearance-Settings.extension"
 echo "\nPress any key once complete"
 read
+echo "\n\nBlock Spotlight from indexing Documents"
+open "x-apple.systempreferences:com.apple.Siri-Settings.extension"
+echo "\nPress any key once complete"
+read
 echo "\n\nMake dock auto hide\nDisable recent items\nTurn off automatically rearranging spaces"
 open "x-apple.systempreferences:com.apple.Desktop-Settings.extension"
+echo "\nPress any key once complete"
+read
+echo "\n\nAdd fingerprints"
+open "x-apple.systempreferences:com.apple.Touch-ID-Settings.extension"
+echo "\nPress any key once complete"
+read
+echo "\n\nSet keyboard repeat rate to 8/8\nSet delay until repeat to 6/6\nEnable Keyboard Navigation\nOpen Keyboard Shortcuts\nDisable spotlight shortcut\nDisable caps-lock for both keyboards"
+open "x-apple.systempreferences:com.apple.Keyboard-Settings.extension"
 echo "\nPress any key once complete"
 read
 echo "\n\nSet mouse tracking speed to 7/10\nEnable mouse right-click"
