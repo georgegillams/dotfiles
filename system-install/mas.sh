2e8b3948ad3827265681ea9408f70a71aa3f1665
George Gillams <code@georgegillams.co.uk>
2024-01-03 14:07:33 +0000

    chore: Update brew install script

--- system-install/mas.sh
+++ system-install/mas.sh
@@ -4,12 +4,13 @@ read
 
 sudo chown -R $(whoami) /usr/local/lib/pkgconfig
 
+mas install 937984704 # Amphetamine
 mas install 1254981365 # Contrast — Color Accessibility
+mas install 2125818515 # Contraster
 mas install 1470584107 # Dato
 mas install 1444383602 # GoodNotes 6
 mas install 409183694 # Keynote
-mas install 409201541 # Pages
 mas install 409203825 # Numbers
-mas install 937984704 # Amphetamine
+mas install 409201541 # Pages
+mas install 1186159417 # TP-Link Deco
 mas install 1524540481 # Widgy Widgets
-mas install 2125818515 # Contraster
