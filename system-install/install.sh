31a132fc1214deee0992ed56fcac1a0ab3226046
George Gillams <code@georgegillams.co.uk>
2024-01-09 15:43:50 +0000

    feat: Add app onboarding

--- system-install/install.sh
+++ system-install/install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-(. ./software-update.sh)
-(. ./system-preferences.sh)
-(. ./finder.sh)
-# (. ./screensavers.sh)
-(. ./touch_id.sh)
-(. ./brew.sh)
-(. ./cldt.sh)
+(. ./01-software-update.sh)
+(. ./02-system-preferences.sh)
+(. ./03-finder.sh)
+(. ./04-touch_id.sh)
+(. ./05-brew.sh)
+(. ./06-cldt.sh)
 
 # SWITCH TO ITERM
 
-(. ./zsh.sh)
-(. ./firefox.sh)
-(. ./git.sh)
-(. ./nvm.sh)
+(. ./07-zsh.sh)
+(. ./08-firefox.sh)
+(. ./09-git.sh)
+(. ./10-nvm.sh)
 
 # OPEN A NEW TAB
 
-(. ./clone-dotfiles.sh)
-(. ./rbenv.sh)
-(. ./vim.sh)
-(. ./mas.sh)
-(. ./misc.sh)
-(. ./adobe-cc.sh)
-(. ./large-downloads.sh)
-(. ./login-items-and-permissions.sh)
+(. ./11-clone-dotfiles.sh)
+(. ./12-rbenv.sh)
+(. ./13-vim.sh)
+(. ./14-mas.sh)
+(. ./15-misc.sh)
+(. ./16-adobe-cc.sh)
+(. ./17-large-downloads.sh)
+(. ./18-onboard-all-apps.sh)
+(. ./19-login-items-and-permissions.sh)
