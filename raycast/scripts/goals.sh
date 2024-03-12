90eec3d85fc6de413078b0746531eba27161ad49
George Gillams <code@georgegillams.co.uk>
2024-03-12 16:49:50 +0000

    fix: Update raycast scripts

--- raycast/scripts/goals.sh
+++ raycast/scripts/goals.sh
@@ -12,9 +12,9 @@
 # @raycast.author George Gillams
 # @raycast.authorURL https://www.georgegillams.co.uk/
 
-open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/2_Areas/Growth, Goals, and Dreams/Start here.pages"
-open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/2_Areas/Work/Typeform/Performance/Promotion/Career framework competencies assesement.numbers"
-open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/2_Areas/Growth, Goals, and Dreams/Dreams by Dimensions.pages"
-open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/2_Areas/Growth, Goals, and Dreams/Accountability chart.numbers"
-open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/2_Areas/Life planning/Life plan.numbers"
-open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-g@georgegillams.co.uk/My Drive/3_Resources/GoodNotes templates/Accountability chart/Accountability chart.pages"
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-hello@georgegillams.co.uk/My Drive/2_Areas/Growth, Goals, and Dreams/Start here.pages"
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-hello@georgegillams.co.uk/My Drive/2_Areas/Work/Typeform/Performance/Promotion/Career framework competencies assesement.numbers"
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-hello@georgegillams.co.uk/My Drive/2_Areas/Growth, Goals, and Dreams/Dreams by Dimensions.pages"
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-hello@georgegillams.co.uk/My Drive/2_Areas/Growth, Goals, and Dreams/Accountability chart.numbers"
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-hello@georgegillams.co.uk/My Drive/2_Areas/Life planning/Life plan.numbers"
+open "/Users/george.gillams/Library/CloudStorage/GoogleDrive-hello@georgegillams.co.uk/My Drive/3_Resources/GoodNotes templates/Accountability chart/Accountability chart.pages"
