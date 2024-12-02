49f99506997ce2bf9449ca07fc77f5416a2053fc
George Gillams <code@georgegillams.co.uk>
2024-12-02 09:10:36 +0000

    chore: Update brew install script

--- system-install/12-asdf.sh
+++ system-install/12-asdf.sh
@@ -1,3 +1,4 @@
 echo "nodejs 22.11.0" > ~/.tool-versions
+asdf install nodejs 22.11.0
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add yarn https://github.com/twuni/asdf-yarn.git
