31c705eaf7363389e91f932442fa26956fa99fbf
George Gillams <code@georgegillams.co.uk>
2025-01-07 15:25:51 +0000

    fix: Fix gh job cancel for multiple jobs

--- gh-cancel-jobs.js
+++ gh-cancel-jobs.js
@@ -7,9 +7,9 @@ const currentBranch = execSync("git rev-parse --abbrev-ref HEAD")
 
 const ghQueryCommand = `gh run list --jq ".[] | select (.status != \\"completed\\") | select (.headBranch == \\"${currentBranch}\\")" --json status,headBranch,databaseId`;
 console.log(`Running command: ${ghQueryCommand}`);
-const jobsString = execSync(ghQueryCommand).toString();
-console.log(`Jobs: ${jobsString}`);
-const jobs = JSON.parse(`[${jobsString}]`);
+const jobsStrings = execSync(ghQueryCommand).toString().split('\n');
+console.log(`\nJobs:\n${jobsStrings.join('\n')}\n`);
+const jobs = JSON.parse(`[${jobsStrings.join(',')}]`);
 
 for (const job of jobs) {
   console.log(`cancelling job ${job.databaseId}`);
