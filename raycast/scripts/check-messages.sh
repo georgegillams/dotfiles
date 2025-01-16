dd0254f30a9317194ac440454e141625e39dbcbd
George Gillams <code@georgegillams.co.uk>
2025-01-16 11:55:21 +0000

    chore: Update ZSHRC

---
+++ raycast/scripts/check-messages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Check messages
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📥
+
+# Documentation:
+# @raycast.author George Gillams
+# @raycast.authorURL https://www.georgegillams.co.uk/
+
+osascript "/Users/george.gillams/Documents/georgegillams/dotfiles/apple script/open_message_apps.scpt"
---
+++ raycast/scripts/run-background-apps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Run Background Apps
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🚀
+
+# Documentation:
+# @raycast.author George Gillams
+# @raycast.authorURL https://www.georgegillams.co.uk/
+
+osascript "/Users/george.gillams/Documents/georgegillams/dotfiles/apple script/open_background_apps.scpt"
