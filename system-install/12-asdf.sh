19f9997908b19d2b17cf76dc848542fd8183dfb0
George Gillams <code@georgegillams.co.uk>
2025-03-26 20:03:28 +0000

    fix: Update asdf install

--- system-install/12-asdf.sh
+++ system-install/12-asdf.sh
@@ -1,4 +1,8 @@
 echo "nodejs 22.11.0" > ~/.tool-versions
-asdf install nodejs 22.11.0
+open https://github.com/asdf-vm/asdf
+mkdir -p $HOME/asdf/bin
+open -p $HOME/asdf/bin
+echo "Download ASDF and place binary in $HOME/asdf/bin"
+echo "Download asdf.sh and place in $HOME/asdf/asdf.sh"
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add yarn https://github.com/twuni/asdf-yarn.git
