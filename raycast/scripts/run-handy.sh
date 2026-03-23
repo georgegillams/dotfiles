71dbd4b370b815a31282af44ddb7071856af7dd2
George Gillams <code@georgegillams.co.uk>
2026-03-23 10:05:09 +0000

    chore: Update brew install script

---
+++ raycast/scripts/run-handy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Run Handy
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ✋
+
+# Documentation:
+# @raycast.author George Gillams
+# @raycast.authorURL https://www.georgegillams.co.uk/
+
+pkill handy
+sleep 1
+open /Applications/Handy.app
--- system-install/16-mas.sh
+++ system-install/16-mas.sh
@@ -11,11 +11,13 @@ mas install 937984704 # Amphetamine
 # mas install 1254981365 # Contrast — Color Accessibility
 # mas install 2125818515 # Contraster
 mas install 1444383602 # GoodNotes 6
-mas install 409183694 # Keynote
-mas install 409203825 # Numbers
-mas install 409201541 # Pages
 mas install 1593408455 # Anybox
 mas install 1891057991 # Folder Preview
+mas install 409183694 # Keynote
+mas install 409201541 # Pages
+mas install 409203825 # Numbers
+mas install 6450280202 # Day Progress
+mas install 6754022187 # Menu Drop
 # mas install 1524540481 # Widgy Widgets
 
 if [[ $setup_type == "WORK" ]]; then
