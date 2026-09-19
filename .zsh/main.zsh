72172ca45fdf00630f5150b9a811de7d4db189d8
George Gillams <code@georgegillams.co.uk>
2026-09-19 08:57:45 +0100

    fix: Kubectl

--- .zsh/main.zsh
+++ .zsh/main.zsh
@@ -96,9 +96,9 @@ fi
 export PATH=~/usr/bin:/bin:/usr/sbin:/sbin:~/go/bin/:$PATH
 
 # Auto-attach to tmux in every new iTerm2 window/tab
-if [[ "$TERM_PROGRAM" == "iTerm.app" ]] && command -v tmux &>/dev/null && [[ -z "$TMUX" ]]; then
-  tmux new-session
-fi
+# if [[ "$TERM_PROGRAM" == "iTerm.app" ]] && command -v tmux &>/dev/null && [[ -z "$TMUX" ]]; then
+#   tmux new-session
+# fi
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
